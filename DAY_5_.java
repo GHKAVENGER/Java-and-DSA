2ac08ef6d7c97f9e3bfff133286d523f9c4e2c8c
GHKAVENGER <hemanthavenger2006@gmail.com>
2025-07-20 22:58:19 +0530

    DAY_5_Time_And_Space_Complexity

---
+++ DAY_5_.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class DAY_5_ {
+    public static void main(String[] args) {
+     Scanner scan=new Scanner(System.in);
+     
+     int a=scan.nextInt();
+     System.out.println(a);
+
+    }
+    
+}
